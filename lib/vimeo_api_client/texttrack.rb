4230952b84eca5fc99cfcae143e264f0c5f1d155
SergeyMell <sergey.mell@agilie.com>
2017-12-06 01:25:39 +0200

    Implemented text tracks uploading

--- lib/vimeo_api_client/texttrack.rb
+++ lib/vimeo_api_client/texttrack.rb
@@ -1,9 +1,34 @@
+require 'vimeo_api_client/client'
+
 module Vimeo
   class TextTrack
+
+    include Client
 
     def initialize(id = nil, video_id)
       @id = id
       @video_id = video_id
+    end
+
+    def index
+      request("/videos/#{@video_id}/texttracks")
+    end
+
+    def get
+      request("/videos/#{@video_id}/texttracks/#{@id}")
+    end
+
+    # type
+    # language
+    # name
+    def create(options)
+      response = request("/videos/#{@video_id}/texttracks", { body: options }, :post)
+      response = upload_file(response.link, options[:file]) if options[:file]
+      response
+    end
+
+    def upload_file(upload_link, file)
+      HTTParty.put(upload_link, body: file)
     end
 
   end
