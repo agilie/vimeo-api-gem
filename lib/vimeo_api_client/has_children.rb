ba6e9997ed6c44bc5b26c274771f4facc25de81a
SergeyMell <sergey.mell@agilie.com>
2017-12-25 23:15:11 +0200

    Some refactoring

--- lib/vimeo_api_client/has_children.rb
+++ lib/vimeo_api_client/has_children.rb
@@ -9,8 +9,7 @@ module Vimeo
 
       define_method "#{resource_plural}" do |uri = nil|
         klass = Object.const_get "Vimeo::#{snake_to_camel(singularize(resource_plural))}"
-        class_name = self.class.class_name
-        parents = self.parents.merge("#{class_name}_id".to_sym => self.id)
+        parents = self.parents.merge(self.class.resource_id => self.id)
         klass.new(uri, parents)
       end
 
--- lib/vimeo_api_client/resource.rb
+++ lib/vimeo_api_client/resource.rb
@@ -10,7 +10,6 @@ module Vimeo
     attr_reader :id, :parents
 
     def initialize(uri = nil, parents = {})
-      uri = uri.to_s
       @id = get_id(uri)
       @parents = get_parents(uri)
       @parents.merge!(parents)
@@ -26,10 +25,6 @@ module Vimeo
 
     protected
 
-    def self.descendants
-      ObjectSpace.each_object(Class).select {|klass| klass < self}
-    end
-
     def get_id(uri)
       uri[/^\d+$/] || self.class.get_resource_id(uri)
     end
@@ -37,18 +32,20 @@ module Vimeo
     def get_parents(uri)
       parents = {}
       (Vimeo::Resource.descendants - [self.class]).each do |klass|
-        klass_name = klass.class_name
         resource_id = klass.get_resource_id(uri)
         next if resource_id.nil? || resource_id.empty?
-        key = "#{klass_name}_id".to_sym
-        parents[key] = resource_id
+        parents[klass.resource_id] = resource_id
       end
       parents
     end
 
+    def self.descendants
+      ObjectSpace.each_object(Class).select {|klass| klass < self}
+    end
+
     def self.get_resource_id(uri)
       regex = Regexp.new("#{resource_name}/(\\d+)")
-      match = uri.match(regex)
+      match = uri.to_s.match(regex)
       match[1] if match
     end
 
@@ -58,6 +55,10 @@ module Vimeo
 
     def self.resource_name
       "#{class_name}s"
+    end
+
+    def self.resource_id
+      "#{class_name}_id".to_sym
     end
 
   end
