76d8ecc9a9253985aac1801fd89ec240c504707a
SergeyMell <sergey.mell@agilie.com>
2017-12-27 01:53:45 +0200

    Implemented actions for updating by upload ticket generation and user form

--- lib/vimeo_api_client/video.rb
+++ lib/vimeo_api_client/video.rb
@@ -9,19 +9,19 @@ module Vimeo
     has_many :thumbnails
 
     def show
-      get("/videos/#{@id}")
+      get("/videos/#{id}")
     end
 
     def update(options = {})
-      patch("/videos/#{@id}", options)
+      patch("/videos/#{id}", options)
     end
 
     def update_timeline_events(options)
-      patch("/videos/#{@id}/timelineevents", options)
+      patch("/videos/#{id}/timelineevents", options)
     end
 
     def destroy
-      delete("/videos/#{@id}")
+      delete("/videos/#{id}")
     end
 
     def create_by_pulling(link)
@@ -29,11 +29,15 @@ module Vimeo
     end
 
     def update_by_pulling(link)
-      put("/videos/#{@id}/files", type: 'pull', link: link)
+      put("/videos/#{id}/files", type: 'pull', link: link)
     end
 
-    def generate_upload_ticket(redirect_url)
+    def create_by_posting(redirect_url)
       post('/me/videos', type: 'post', redirect_url: redirect_url)
+    end
+
+    def update_by_posting(redirect_url)
+      put("/videos/#{id}/files", type: 'pull', redirect_url: redirect_url)
     end
 
     def create_as_resumable
